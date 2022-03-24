ff2c5eee64de45ffca2376598cae6e455131e9a1
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-24 18:53:25 +0800

    1.1.0

--- bin/cli.ts
+++ bin/cli.ts
@@ -16,13 +16,15 @@ process.on("unhandledRejection", (err) => {
 });
 console.log("tsp2json");
 console.log("usage:");
-console.log("tsp2json --inputdir=path/to/TSP --outputdir=path/to/json");
+console.log(
+    "tsp2json --inputdir=path/to/TSP --outputdir=path/to/json --emptyoutdir=true"
+);
 const extension = "tsp";
 const args = process.argv.slice(2);
 const opts = parse(args);
 
 console.log(opts);
-const { inputdir, outputdir } = opts;
+const { inputdir, outputdir, emptyoutdir } = opts;
 assert(typeof inputdir === "string", "arugments inputdir should not empty");
 assert(typeof outputdir === "string", "arugments outputdir should not empty");
 
@@ -35,36 +37,17 @@ async function start(inputdir: string, outputdir: string) {
     await fsextra.ensureDir(outputdir);
     console.log("递归查找文件...", dirpath);
     //return
-    const files = await findfiles(extreg, dirpath);
+    const files: string[] = await findfiles(extreg, dirpath);
 
     //.then((files) => {
     console.log("文件列表", files);
     const successfiles: string[] = [];
     const failurefiles: string[] = [];
-    // await fsextra.emptyDir(outputdir)
+    if (emptyoutdir === "true") {
+        await fsextra.emptyDir(outputdir);
+    }
     for (let file of files) {
-        try {
-            const basename = path.basename(file, ".tsp");
-            const tsp = (await fsextra.readFile(file)).toString();
-            console.log("读取文件", file);
-            const json = tsp2json(tsp);
-            assertIsArray(json);
-            const outputfile = path.join(
-                outputdir,
-                String(json.length),
-                basename + ".json"
-            );
-            await fsextra.ensureDir(path.join(outputdir, String(json.length)));
-            await fsextra.writeJSON(outputfile, json);
-            console.log("写入文件", outputfile);
-            // console.log(json);
-            successfiles.push(file);
-        } catch (e) {
-            console.error("error in file:" + file);
-            console.error(e);
-
-            failurefiles.push(file);
-        }
+        await processonefile({ file, outputdir, successfiles, failurefiles });
     }
     if (failurefiles.length) {
         console.warn("failurefiles", failurefiles);
@@ -74,5 +57,40 @@ async function start(inputdir: string, outputdir: string) {
         throw new Error("all files failure");
     } else {
         console.log("successfiles", successfiles);
+        console.log(`files ${successfiles.length} succeed`);
+    }
+}
+async function processonefile({
+    file,
+    outputdir,
+    successfiles,
+    failurefiles,
+}: {
+    file: string;
+    outputdir: string;
+    successfiles: string[];
+    failurefiles: string[];
+}) {
+    try {
+        const basename = path.basename(file, ".tsp");
+        const tsp = (await fsextra.readFile(file)).toString();
+        console.log("读取文件", file);
+        const json = tsp2json(tsp);
+        assertIsArray(json);
+        const outputfile = path.join(
+            outputdir,
+            String(json.length),
+            basename + ".json"
+        );
+        await fsextra.ensureDir(path.join(outputdir, String(json.length)));
+        await fsextra.writeJSON(outputfile, json);
+        console.log("写入文件", outputfile);
+        // console.log(json);
+        successfiles.push(file);
+    } catch (e) {
+        console.error("error in file:" + file);
+        console.error(e);
+
+        failurefiles.push(file);
     }
 }
