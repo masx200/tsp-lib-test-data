c44fe57b4ce8f364316d7f6d29a9e7ad8ab3e998
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-24 13:52:16 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,27 @@
 import { asserttrue } from "../test/asserttrue";
 
-const modules = import.meta.globEager("../lib/*.json");
+const modules = import.meta.glob("../lib/*.json");
 export type NodeCoordinates = Array<[number, number]>;
 // console.log(modules);
-const TSP_cords: Record<string, NodeCoordinates> = {
+const TSP_cords: Record<string, () => Promise<NodeCoordinates>> =
+    // {
     // node_coordinates10,
     // node_coordinates16,
     // node_coordinates12,
     // node_coordinates21,
     // node_coordinates30,
-    ...Object.fromEntries(
+    // ...
+    Object.fromEntries(
         Object.entries(modules).map(([key, value]) => {
             const name = key
                 .slice(0, key.lastIndexOf("."))
                 .slice(key.lastIndexOf("/") + 1);
             asserttrue(name.length > 0);
-            return [name, value.default];
+            const getcoordinates: () => Promise<NodeCoordinates> = Reflect.get(value, 'default') as () => Promise<NodeCoordinates>
+            asserttrue(typeof getcoordinates === 'function')
+            return [name, getcoordinates];
         })
-    ),
-};
+    )
+// };
 asserttrue(Object.keys(TSP_cords).length);
 export default TSP_cords;
