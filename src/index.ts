808ac4009c456e8f47b3621425cbeef3bbe965b2
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-24 12:43:44 +0800

    index

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { asserttrue } from "../test/asserttrue";
+
+const modules = import.meta.globEager("../lib/*.json");
+export type NodeCoordinates = Array<[number, number]>;
+// console.log(modules);
+const TSP_cords: Record<string, NodeCoordinates> = {
+    // node_coordinates10,
+    // node_coordinates16,
+    // node_coordinates12,
+    // node_coordinates21,
+    // node_coordinates30,
+    ...Object.fromEntries(
+        Object.entries(modules).map(([key, value]) => {
+            const name = key
+                .slice(0, key.lastIndexOf("."))
+                .slice(key.lastIndexOf("/") + 1);
+            asserttrue(name.length > 0);
+            return [name, value.default];
+        })
+    ),
+};
+export default TSP_cords;
