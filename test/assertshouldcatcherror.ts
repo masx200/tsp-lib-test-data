225be9c239791a04d8e719597f380d594be18ef7
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-24 19:03:49 +0800

    测试

--- test/assertshouldcatcherror.ts
+++ test/assertshouldcatcherror.ts
@@ -1,8 +1,16 @@
-export function assertshouldcatcherror(fn: () => void): boolean {
+export function assertshouldcatcherror(fn: () => void, msg?: string): boolean {
     try {
         fn();
     } catch (error) {
-        return true;
+        const message = String((error as any)?.message);
+
+        if (msg) {
+            return message.includes(msg);
+        } else {
+            if (message) {
+                return true;
+            }
+        }
     }
     throw Error("assert shouldcatcherror");
 }
